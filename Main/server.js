e7d3769104b48a5812751e8547869235fb26d799
Matt Lahey <mdlahey@outlook.com>
2022-12-08 18:37:45 -0500

    fixed bug with dependencies and mySQL db connection

--- Main/server.js
+++ Main/server.js
@@ -1,5 +1,5 @@
 // Add routes/dependencies
-const connection = require('.Main/config/connection');
+const connection = require('./config/connection');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const figlet = require('figlet');
@@ -9,14 +9,15 @@ const { throwError } = require('rxjs');
 // Establish Connection to mySQL and Command Line Introduction Text
 connection.connect((error) => {
     if (error) throw error;
-    console.log(chalk.cyan.bold('===================================================================='));
+    console.log(chalk.cyan.bold('================================================================================================================================'));
     console.log('');
     console.log(chalk.cyan.bold(figlet.textSync('Employee Tracker Database')));
     console.log('');
-    console.log(chalk.cyan.bold('===================================================================='));
+    console.log(chalk.cyan.bold('================================================================================================================================'));
+    console.log('Connection to employee_db established')
     promptUser();
 })
 
 const promptUser = () => {
-    console.log('Connection to database established')
+
 }
